2a863a047cb65a3d4e3386a061dfd067abffcae7
slebetman <slebetman@gmail.com>
2022-03-15 17:26:39 +0800

    Bootstraping React

---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+	template: "./public/index.html",
+	filename: "./index.html"
+});
+
+module.exports = {
+	mode: "development",
+	output: {
+		path: path.resolve(__dirname, "build")
+	},
+	module: {
+		rules: [{
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loader: "babel-loader",
+			options: {
+				presets: ['@babel/preset-react']
+			}
+		},
+		{
+			test: /\.css$/,
+			use: ["style-loader", "css-loader"]
+		}]
+	},
+	plugins: [htmlPlugin],
+	devServer: {
+		compress: true,
+		allowedHosts: 'all'
+	}
+};
